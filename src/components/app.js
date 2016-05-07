e40cb9d8ff85f11c548901c38c50805e940e28eb
Tyrantz <tyrantwarzhip@gmail.com>
2016-05-07 17:22:06 -0500

    listens for keys being pressed

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ScoreBoard from './score-board';
 import GameWorld from './game-world';
+import '../../styles/style.scss';
+import keydown from 'react-keydown';
 
-export default () => {
-  return (
-    <div>
-      <ScoreBoard />
-      <GameWorld />
-    </div>
-  );
+@keydown
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // store keydown: event in event constant
+    const { keydown: { event } } = nextProps;
+    if (event) {
+      switch (event.which) {
+        case 37:
+          console.log('move left');
+          break;
+        case 38:
+          console.log('move top');
+          break;
+        case 39:
+          console.log('move right');
+          break;
+        case 40:
+          console.log('move bot');
+          break;
+
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ScoreBoard />
+        <GameWorld />
+      </div>
+    );
+  }
 }
--- src/components/game-world.js
+++ src/components/game-world.js
@@ -1,17 +1,28 @@
-import React from 'react';
-/* require('../../styles/style.scss'); */
-import '../../styles/style.scss';
+import React, { Component } from 'react';
+/* import keydown from 'react-keydown'; */
+import Player from './player';
 
-export default () => {
-  return (
-    <div className="game-container">
-      <div className="area-great-hall">
+export default class GameWorld extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { key: 'n/a' };
+  }
+
+
+  render() {
+    return (
+      <div className="game-container">
+        <div className="area-great-hall">
+        </div>
+        <div className="area-sleeping-quarters"></div>
+        <div className="area-barracks"></div>
+        <div className="area-kings-room"></div>
+        <div className="bridge-1"></div>
+        <div className="bridge-2"></div>
+        <Player />
       </div>
-      <div className="area-sleeping-quarters"></div>
-      <div className="area-barracks"></div>
-      <div className="area-kings-room"></div>
-      <div className="bridge-1"></div>
-      <div className="bridge-2"></div>
-    </div>
-  );
+    );
+  }
 }
+
