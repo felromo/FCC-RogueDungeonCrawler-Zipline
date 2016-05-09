266e544a9232329e7a703667b1efd5981c5de173
Tyrantz <tyrantwarzhip@gmail.com>
2016-05-09 18:47:09 -0500

    turns player back into a functional component

--- src/components/player.js
+++ src/components/player.js
@@ -1,22 +1,10 @@
 import React,{Component} from 'react';
 import $ from 'jquery';
 
-// 37 -left
-// 38 - top
-// 39 - right
-// 40 - bot
 
-export default class Player extends Component {
+export default () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {x: 0,
-                  y: 0};
-  }
-
-  render() {
     return (
       <div className="player"></div>
     );
-  }
 }
