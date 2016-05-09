f278797886b59bad8adce00d2f4ad8819af6d3b3
Tyrantz <tyrantwarzhip@gmail.com>
2016-05-09 18:47:38 -0500

    replaces react-down with jquery keydown listeners

--- src/components/app.js
+++ src/components/app.js
@@ -2,41 +2,78 @@ import React, {Component} from 'react';
 import ScoreBoard from './score-board';
 import GameWorld from './game-world';
 import '../../styles/style.scss';
-import keydown from 'react-keydown';
+import $ from 'jquery';
 
-@keydown
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      health: 100,
+      level: 1,
+      weapon: 'dagger'
+    };
+    this.player = {
+      x: 0,
+      y: 0
+    };
+    this.keysPressed = {};
+    this.movementRate = 1;
+    this.calculateNewPosition = this.calculateNewPosition.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
-    // store keydown: event in event constant
-    const { keydown: { event } } = nextProps;
-    if (event) {
-      switch (event.which) {
-        case 37:
-          console.log('move left');
-          break;
-        case 38:
-          console.log('move top');
-          break;
-        case 39:
-          console.log('move right');
-          break;
-        case 40:
-          console.log('move bot');
-          break;
-
-      }
-    }
+  calculateNewPosition(oldValue, direction1, direction2) {
+    return parseInt(oldValue, 10)
+                 - (this.keysPressed[direction1] ? this.movementRate : 0)
+                 + (this.keysPressed[direction2] ? this.movementRate : 0);
   }
+
+  componentDidMount() {
+    // after we've mounted the componnet start listening for keystrokes
+    $(window).keydown(event => {
+      this.keysPressed[event.which] = true;
+    });
+    $(window).keyup(event => {
+      this.keysPressed[event.which] = false;
+    });
+    setInterval(() => {
+      // every 20 milliseconds update players position on screen and store those values
+      $('.player').css({
+        left: (index) => {
+          // left and right
+          return this.calculateNewPosition(this.player.x, 37, 39);
+        },
+        top: (index)  => {
+          // up and down
+          return this.calculateNewPosition(this.player.y, 38, 40);
+        }
+      });
+      this.player.x = parseInt($('.player').css('left'));
+      this.player.y = parseInt($('.player').css('top'));
+    }, 20);
+  }
+
 
   render() {
     return (
       <div>
-        <ScoreBoard />
+        <ScoreBoard
+          health={this.state.health}
+          level={this.state.level}
+          weapon={this.state.weapon}/>
         <GameWorld />
+        <button
+          onClick={()=> { this.setState({health: this.state.health-1});}}>
+          taking damage!
+        </button>
+        <button
+          onClick={() => {this.setState({level: this.state.level+1});}}>
+          level up
+        </button>
+        <button
+          onClick={() => {this.setState({weapon: 'sword'});}}>
+          gain weapon
+        </button>
+        <button onClick={() => {console.log(this.player.x,this.player.y);}}>get player location</button>
       </div>
     );
   }
